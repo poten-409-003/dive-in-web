1c0a5ec727dfcfd63269f2a91600c1dff19b2e67
byunghun-ben <figma@kakao.com>
2024-09-30 18:02:13 +0900

    리다이렉트 페이지에 대한 메타정보 체크   - 동적 메타정보 생성방식

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import { Metadata } from "next";
 import { redirect } from "next/navigation";
 
-export const metadata: Metadata = {
-  title: "Dive In",
-  description: "수영에 빠져 빠져 빠져!",
+export const generateMetadata = () => {
+  return {
+    title: "Dive In",
+    description: "수영에 빠져 빠져 빠져!",
+  };
 };
 
 export default function Home() {
