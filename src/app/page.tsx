c5e566a0bf94a1d61fc5f833430c7fc30d4c7907
byunghun-ben <figma@kakao.com>
2024-09-30 17:54:02 +0900

    리다이렉트 페이지에 대한 메타정보 체크

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from "next";
 import { redirect } from "next/navigation";
+
+export const metadata: Metadata = {
+  title: "Dive In",
+  description: "수영에 빠져 빠져 빠져!",
+};
 
 export default function Home() {
   redirect("/lessons");
