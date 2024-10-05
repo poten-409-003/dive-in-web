34caaf3506010d4f1adbca0351f007939f178a74
byunghun-ben <figma@kakao.com>
2024-10-05 11:24:25 +0900

    feat: 카카오 로그인 에러 내용 확인 가능하도록 변경

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -8,29 +8,35 @@ export async function GET(request: Request) {
     return NextResponse.redirect(`${origin}/auth/login?error=invalid_code`);
   }
 
-  // const url = new URL(`${origin}/api/auth/login`);
-  const url = new URL(`http://localhost:8080/login/kakao`);
-  url.searchParams.append("code", code);
-  url.searchParams.append("redirect_uri", `${origin}/api/auth/callback`);
+  try {
+    // const url = new URL(`${origin}/api/auth/login`);
+    const url = new URL(`http://localhost:8080/login/kakao`);
+    url.searchParams.append("code", code);
+    url.searchParams.append("redirect_uri", `${origin}/api/auth/callback`);
 
-  const res = await fetch(url);
+    const res = await fetch(url);
 
-  if (!res.ok) {
-    return NextResponse.redirect(`${origin}/auth/login?error=failed_to_login`);
+    if (!res.ok) {
+      return NextResponse.redirect(
+        `${origin}/auth/login?error=failed_to_login`
+      );
+    }
+
+    const { access_token, refresh_token } = await res.json();
+
+    return NextResponse.redirect(`${origin}`, {
+      headers: [
+        [
+          "Set-Cookie",
+          `access_token=${access_token}; Path=/; HttpOnly; SameSite=Strict`,
+        ],
+        [
+          "Set-Cookie",
+          `refresh_token=${refresh_token}; Path=/; HttpOnly; SameSite=Strict`,
+        ],
+      ],
+    });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
   }
-
-  const { access_token, refresh_token } = await res.json();
-
-  return NextResponse.redirect(`${origin}`, {
-    headers: [
-      [
-        "Set-Cookie",
-        `access_token=${access_token}; Path=/; HttpOnly; SameSite=Strict`,
-      ],
-      [
-        "Set-Cookie",
-        `refresh_token=${refresh_token}; Path=/; HttpOnly; SameSite=Strict`,
-      ],
-    ],
-  });
 }
