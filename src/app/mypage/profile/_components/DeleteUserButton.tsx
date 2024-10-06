1c7c320bf772e652d7ed594d45a80279a752f92e
byunghun-ben <figma@kakao.com>
2024-10-06 11:47:19 +0900

    fix: 디자인 qa   - 모달

--- src/app/mypage/profile/_components/DeleteUserButton.tsx
+++ src/app/mypage/profile/_components/DeleteUserButton.tsx
@@ -2,69 +2,73 @@
 
 import { revalidateTagAction } from "@/actions/revalidate";
 import {
+  Description,
   Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
+  DialogBackdrop,
+  DialogPanel,
   DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
+} from "@headlessui/react";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const DeleteUserButton = () => {
   const router = useRouter();
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <Dialog>
-      <DialogTrigger className="flex items-center justify-center p-3 h-10 text-body_sm text-gray-500">
+    <>
+      <button
+        className="flex items-center justify-center p-3 h-10 text-body_sm text-gray-500"
+        onClick={() => {
+          setIsOpen(true);
+        }}
+      >
         회원 탈퇴
-      </DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle className="text-left text-body_lb text-gray-900">
-            회원 탈퇴
-          </DialogTitle>
-          <DialogDescription>정말 탈퇴하시겠어요?</DialogDescription>
-        </DialogHeader>
+      </button>
+      <Dialog
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+        className="relative z-50"
+      >
+        {/* The backdrop, rendered as a fixed sibling to the panel container */}
+        <DialogBackdrop className="fixed inset-0 bg-black/70" />
 
-        <div className="flex gap-2">
-          <DialogClose asChild>
-            <button className="flex-1 flex items-center justify-center h-12 bg-gray-200 rounded-lg text-body_sm text-gray-800">
-              취소
-            </button>
-          </DialogClose>
-
-          <button
-            className="flex-1 flex items-center justify-center h-12 bg-primary rounded-lg text-body_sm text-primary-foreground"
-            onClick={async () => {
-              await fetch("/api/auth/user", {
-                method: "DELETE",
-              });
-              revalidateTagAction("user");
-              router.replace("/");
-            }}
-          >
-            탈퇴
-          </button>
+        {/* Full-screen container to center the panel */}
+        <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
+          {/* The actual dialog panel  */}
+          <DialogPanel className="max-w-lg w-full space-y-4 bg-white flex flex-col rounded-2xl overflow-hidden">
+            <div className="flex flex-col gap-2 px-4 pt-5">
+              <DialogTitle className="font-bold text-body_lb">
+                회원 탈퇴
+              </DialogTitle>
+              <Description className="text-body_br text-gray-600">
+                정말 탈퇴하시겠어요?
+              </Description>
+            </div>
+            <div className="flex gap-2 px-4 pb-5">
+              <button
+                onClick={() => setIsOpen(false)}
+                className="flex-1 flex items-center justify-center h-12 bg-gray-200 rounded-lg text-body_sm text-gray-800"
+              >
+                취소
+              </button>
+              <button
+                onClick={async () => {
+                  await fetch("/api/auth/user", {
+                    method: "DELETE",
+                  });
+                  revalidateTagAction("user");
+                  router.replace("/");
+                }}
+                className="flex-1 flex items-center justify-center h-12 bg-primary rounded-lg text-body_sm text-primary-foreground"
+              >
+                탈퇴
+              </button>
+            </div>
+          </DialogPanel>
         </div>
-      </DialogContent>
-    </Dialog>
-  );
-
-  return (
-    <button
-      className="flex items-center justify-center p-3 h-10 text-body_sm text-gray-500"
-      onClick={async () => {
-        await fetch("/api/auth/user", {
-          method: "DELETE",
-        });
-        revalidateTagAction("user");
-        router.replace("/");
-      }}
-    >
-      회원 탈퇴
-    </button>
+      </Dialog>
+    </>
   );
 };
 
