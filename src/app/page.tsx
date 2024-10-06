d3b91b72d74c815d6456e46c99e5a83891cec765
byunghun-ben <figma@kakao.com>
2024-10-06 13:56:21 +0900

    렌딩 이미지 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,10 +11,10 @@ export default function Home() {
   }, [router]);
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-primary">
+    <div className="flex flex-col items-center justify-center h-screen bg-white">
       <Image
         alt="로고"
-        src="/image/logo_b.png"
+        src="/image/logo_w.png"
         width={200}
         height={200}
         priority
