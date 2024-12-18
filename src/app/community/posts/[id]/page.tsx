fba339607f81e5ec93cb2dc5569cee87d2294799
Minep <pmh@ge.kornu.ac.kr>
2024-12-19 00:38:28 +0900

    fix: postId number에서 string으로 타입 변환

--- src/app/community/posts/[id]/page.tsx
+++ src/app/community/posts/[id]/page.tsx
@@ -3,7 +3,7 @@ import { notFound } from "next/navigation";
 import ClientCommunityPage from "./clientPage";
 
 const CommunityPage = async ({ params }: { params: { id: string } }) => {
-  const CommunityId = Number(params.id);
+  const CommunityId = params.id;
   const community = await getCommunity(CommunityId);
 
   if (!community) {
