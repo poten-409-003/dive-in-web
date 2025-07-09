3c33b2b611e3aff6100bda5beabed8b9fd8a54fd
Minep <pmh@ge.kornu.ac.kr>
2025-07-09 20:14:42 +0900

    fix: 백엔드 수정으로 인해 필요없는 부분 주석처리

--- src/schemas/lessons.ts
+++ src/schemas/lessons.ts
@@ -15,6 +15,25 @@ export const lessonSchema = z.object({
   price: z.string().nullable(),
 });
 
+// export const lessonDetailContentSchema = z.object({
+//   classTopic: z.string().default(""),
+//   eligibilityRequirements: z.array(z.string()).default([]),
+//   classIntroduction: z.string().default(""),
+//   applicationMethod: z.array(z.string()).default([]),
+//   refundPolicy: z.array(z.string()).default([]),
+// });
+
+export const lessonDetailContentSchema = z.object({
+    topic: z.string(),
+    eligibilityRequirements: z.array(z.string()).nullable(),
+    introduction: z.string().nullable(),
+    applicationMethod: z.array(z.object({
+      applyUrl: z.string(),
+      applyUrlType: z.string(),
+    })),
+    refundPolicy: z.array(z.string()).nullable(),
+  });
+
 export const lessonDetailSchema = z.object({
   id: z.number(),
   lessonName: z.string().default(""),
@@ -24,8 +43,7 @@ export const lessonDetailSchema = z.object({
   // price: z.string().default(""),
   price: z.string().nullable().default(""),
   keyword: z.string().default(""),
-  lessonDetail: z.string().default(""),
-  // lessonSchedule: z.string().default(""),
+  lessonDetail: lessonDetailContentSchema,
   lessonSchedule: z.string().nullable().default(""),
   lessonStatus: z.string().default(""),
   academy: z.object({
@@ -42,38 +60,53 @@ export const lessonDetailSchema = z.object({
     .object({
       id: z.number(),
       poolName: z.string().default(""),
-      poolAddress: z.string().default(""),
+      poolAddress: z.string().nullable(),
       region: z.string().default(""),
       imageUrl: z.string().default(""),
       latitude: z.number().or(z.string()).pipe(z.coerce.number()),
       longitude: z.number().or(z.string()).pipe(z.coerce.number()),
     })
     .nullable(),
-  instructors: z
-    .object({
-      id: z.number(),
-      instructorId: z.number(),
-      instructorName: z.string().default(""),
-      instructorInfo: z.string().default(""),
-    })
-    .array(),
-  images: z.object({ imageUrl: z.string() }).array(),
-  applyChannels: z
-    .object({
-      applyUrlType: z.string(),
-      applyUrl: z
-        .string()
-        .nullable()
-        .transform((value) => value || ""),
-    })
-    .array(),
-});
+    images: z.object({ imageUrl: z.string() }).array(),
+  });
+
+  // lessonDetail: z.object({
+  //   topic: z.string(),
+  //   eligibilityRequirements: z.array(z.string()).nullable(),
+  //   introduction: z.string(),
+  //   applicationMethod: z.array(z.object({
+  //     applyUrl: z.string(),
+  //     applyUrlType: z.string(),
+  //   })),
+  //   refundPolicy: z.array(z.string()).nullable(),
+  // }),
+  //  }),
+  // lessonDetail: z.string().default(""),
+  // lessonSchedule: z.string().default(""),
+    // instructors: z
+  //   .object({
+  //     id: z.number(),
+  //     instructorId: z.number(),
+  //     instructorName: z.string().default(""),
+  //     instructorInfo: z.string().default(""),
+  //   })
+  //   .array().nullable().optional(),
+  // applyChannels: z
+  //   .object({
+  //     applyUrlType: z.string(),
+  //     applyUrl: z
+  //       .string()
+  //       .nullable()
+  //       .transform((value) => value || ""),
+  //   })
+  //   .array(),
 
 //이 부분이 문제
-export const lessonDetailContentSchema = z.object({
-  classTopic: z.string().default(""),
-  eligibilityRequirements: z.array(z.string()).default([]),
-  classIntroduction: z.string().default(""),
-  applicationMethod: z.array(z.string()).default([]),
-  refundPolicy: z.array(z.string()).default([]),
-});
+// export const lessonDetailContentSchema = z.object({
+//   classTopic: z.string().default(""),
+//   eligibilityRequirements: z.array(z.string()).default([]),
+//   classIntroduction: z.string().default(""),
+//   applicationMethod: z.array(z.string()).default([]),
+//   refundPolicy: z.array(z.string()).default([]),
+// });
+
