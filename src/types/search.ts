182ec40200932fd4454a6c015637abbd5b6a7847
Minep <pmh@ge.kornu.ac.kr>
2025-07-13 21:20:38 +0900

    fix: search  타입 재구성

--- src/types/search.ts
+++ src/types/search.ts
@@ -1,7 +1,13 @@
-export type SearchResult = {
-  id: number;
-  categoryName: string;
-  title: string;
-  address?: string;
-  // icon?: React.ReactNode;
-}
+// export type SearchResult = {
+//   id: number;
+//   categoryName: string;
+//   title: string;
+//   address?: string;
+//   // icon?: React.ReactNode;
+// }
+
+import { searchSchema } from "@/schemas/search";
+import { z } from "zod";
+
+export type Search = z.infer<typeof searchSchema>;
+// export type SearchDetail = z.infer<typeof searchDetailSchema>;
