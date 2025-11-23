86f98bc08e0b038a0a30ae737323e3daffbf2d3f
Minhye Park <61579166+candypmh@users.noreply.github.com>
2025-11-23 23:01:44 +0900

    Refactor: React Query 도입을 위해 전역 Provider 구성

---
+++ src/app/ReactQueryPrivider.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+export default function ReactQueryProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  //QueryClient는 컴포넌트마다 새로 만들면 X 앱 전체에서 1번만 생성
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
